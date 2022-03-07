4a9df92a4561b18e12203c44c254bf4477a86242
LeiProgramming <18681694607@163.com>
2022-03-07 21:52:27 +0800

    spring自研框架环境搭建和HelloWorld项目

---
+++ src/main/java/com/imooc/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.imooc;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Peter
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String name = "My Spring";
+            req.setAttribute("name", name);
+            req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
